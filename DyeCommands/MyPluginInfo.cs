65e6d430e7d792c854586623990c8dc43d58e6d2
Jowdan3006 <jambridge123321@hotmail.com>
2024-12-05 17:26:07 +0000

    plugin info

---
+++ DyeCommands/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace DyeCommands
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "com.16mb.dyecommands";
+        public const string PLUGIN_NAME = "DyeCommands";
+        public const string PLUGIN_VERSION = "0.2.0";
+    }
+}
---
+++ MoreBankTabs/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace MoreBankTabs
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "com.16mb.morebanktabs";
+        public const string PLUGIN_NAME = "MoreBankTabs";
+        public const string PLUGIN_VERSION = "0.1.0";
+    }
+}
